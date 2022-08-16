0c6cf1ba10e098919b9bcf1486d628c1beb36ece
lux <luischn94@gmail.com>
2022-08-16 12:06:36 -0300

    reset db

--- db/migrate/20220731150153_create_products.rb
+++ db/migrate/20220731150153_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :title
       t.integer :views
-
+      t.string :image_link
       t.timestamps
     end
   end
--- db/migrate/20220816133626_add_image_link_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddImageLinkToProducts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :products, :image_link, :string
-  end
-end
